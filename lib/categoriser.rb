25695bb86e5a225a6dba5a1b464d55eddd42069e
Stuart Liston <stuartliston@stuarts-air.lan>
2020-06-01 20:20:39 +1000

    Initialised a new gem

---
+++ lib/categoriser.rb
@@ -0,0 +1,6 @@
+require "categoriser/version"
+
+module Categoriser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/categoriser/version.rb
@@ -0,0 +1,3 @@
+module Categoriser
+  VERSION = "0.1.0"
+end
